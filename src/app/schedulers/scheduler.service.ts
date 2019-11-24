2d859213c252663f388585f2429a71ad10cb939b
kogga <tpkoggalage@gmail.com>
2019-11-24 13:00:14 +0530

    #Fixes

--- src/app/schedulers/scheduler.service.ts
+++ src/app/schedulers/scheduler.service.ts
@@ -8,8 +8,8 @@ import { ScheduleResponse, SchedulerFilterRequest } from './scheduler.model';
 })
 export class SchedulerService {
 
-  private apiTreatmentUrl = 'http://localhost:3000/shedules';
-  // private apiTreatmentUrl = 'schedulers';
+  //private apiTreatmentUrl = 'http://localhost:3000/shedules';
+  private apiSchedulerUrl = 'schedulers';
 
   constructor(
     private baseDataService: BaseDataService
@@ -17,8 +17,8 @@ export class SchedulerService {
 
   public getFilteredScheduleList(request: SchedulerFilterRequest): Observable<Array<ScheduleResponse>> {
     let queryString = `departmentId=${request.departmentId}`;
-    // return this.baseDataService.makeGetCall(`${this.apiTreatmentUrl}${'/filter'}?${queryString}`);
-    return this.baseDataService.makeGetCallTemp(`${this.apiTreatmentUrl}`);
+    return this.baseDataService.makeGetCall(`${this.apiSchedulerUrl}${'/filter'}?${queryString}`);
+    // return this.baseDataService.makeGetCallTemp(`${this.apiTreatmentUrl}`);
   }
 
 }
