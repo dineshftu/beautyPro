2b651e278f67bbd28bf459c0bdb09eb93e911c84
Dinesh Silva <dineshftu@gmail.com>
2019-12-29 15:42:59 +0530

    start ridirect to schedule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'treatments',
+        redirectTo: 'scheduler',
         pathMatch: 'full'
       },
       {
